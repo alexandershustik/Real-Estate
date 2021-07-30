cd64bd06c85b80791d12d08f989b23feab99b9ee
Alexander Shustik <alexandershustik@gmail.com>
2021-07-30 15:09:36 +0200

    nav-menu

--- app/js/app.js
+++ app/js/app.js
@@ -1,13 +1,21 @@
-// // Import jQuery module (npm i jquery)
-// import $ from 'jquery'
-// window.jQuery = $
-// window.$ = $
+// Import jQuery module (npm i jquery)
+import $ from 'jquery';
+window.jQuery = $;
+window.$ = $;
 
 // // Import vendor jQuery plugin example (not module)
 // require('~/app/libs/mmenu/dist/mmenu.js')
 
 document.addEventListener('DOMContentLoaded', () => {
+    // Custom JS
 
-	// Custom JS
-
-})
+    $('.navbar-mobile-button__btn').click(function () {
+        if ($(this).hasClass('navbar-mobile-button__btn--close')) {
+            $(this).removeClass('navbar-mobile-button__btn--close');
+            $('.navbar-menu').removeClass('navbar-menu--show');
+        } else {
+            $(this).addClass('navbar-mobile-button__btn--close');
+            $('.navbar-menu').addClass('navbar-menu--show');
+        }
+    });
+});
